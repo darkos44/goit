ea4316872e367a899e7387a3d330ffc2eb423e8e
Mark <37982535+darkos44@users.noreply.github.com>
2021-08-31 21:04:35 +0300

    Создал файл с третьей домашкой

---
+++ lesson3/hw4.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# path содержит первый аргумент, считаем, что это валидный адрес в файловой системе
+path = sys.argv[1]
+print(f"Start in {path}")
+
+# files - это список имен файлов и папок в path.
+files = os.listdir(path)
