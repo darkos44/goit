127289b1beb0e7975253c9fe1ae498ffa7e2941f
Mark <37982535+darkos44@users.noreply.github.com>
2022-05-06 21:28:52 +0300

    week 07 - 02: dz 7

--- 7 week/m07_02/clean_folder/setup.py
+++ 7 week/m07_02/clean_folder/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='hello_world_lomaster',
+    name='clean_folder',
     version='1.0.0',
-    description='Hello world testing',
+    description='Hw 7 clean folder',
     # url='http://github.com/dummy_user/useful',
     author='Mark Podopryhora',
     author_email='44darkos@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['greeting = hello_world_lomaster.main:greeting']}
+    entry_points={'console_scripts': ['clean_folder = clean_folder.main:start']}
 )
