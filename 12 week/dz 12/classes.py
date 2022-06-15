2c99d282e50905ce5b03dc8d2ebcecdb9226e8f8
Mark <37982535+darkos44@users.noreply.github.com>
2022-06-15 15:42:46 +0300

    dz 12.

---
+++ 12 week/dz 12/classes.py
@@ -0,0 +1,157 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+
+class Field:
+    def __init__(self, value) -> None:
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class Name(Field):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    @Field.value.setter
+    def value(self, name: str):
+        self._value = name
+
+
+class Phone(Field):
+    def __init__(self, phone: str):
+        super().__init__(phone)
+
+    @Field.value.setter
+    def value(self, new_value: str):
+        if not new_value.isdigit():
+            print('Phone must be digit')
+        else:
+            self._value = new_value
+
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, new_value):
+        try:
+            d, m, y = new_value.split('-')
+            date = datetime(day=int(d), month=int(m), year=int(y))
+            self._value = date
+        except ValueError:
+            print('Enter date like 02-05-2022')
+
+
+class Record:
+    def __init__(self, name: Name, adr: Phone, birthday=None):
+        self.Name = name
+        self.Birthday = birthday
+        self.Phones = []
+        if adr != '':
+            self.add_address(adr)
+
+    def add_address(self, adr: Phone):
+        self.Phones.append(adr)
+
+    def delete_address(self, adr: Phone):
+        for p in self.phones:
+            if p.value == adr.value:
+                self.phones.remove(p)
+                return p
+
+    def get_phone(self):
+        result = ""
+        for phone in self.Phones:
+            if result != '':
+                result += ', '
+            result += phone.value
+        return result
+
+    def change_address(self, adr: str):
+        for i in range(len(self.Phones)):
+            if self.Phones[i].value == adr:
+                self.Phones[i].value = adr
+                break
+
+    def days_to_birthday(self):
+        if self.Birthday is None:
+            return 'Birthday day not set'
+        date_now = datetime.now()
+        birthday_date = datetime(year=date_now.year, month=self.Birthday.value.month, day=self.Birthday.value.day)
+        if date_now > birthday_date:
+            birthday_date = datetime(year=date_now.year + 1, month=self.Birthday.value.month,
+                                     day=self.Birthday.value.day)
+        result = birthday_date - date_now
+        return result.days
+
+
+class AddressBook(UserDict):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.count = 0
+        self.load_data()
+
+    def add_record(self, record: Record):
+        self.data[record.Name.value] = record
+
+    def find_record(self, name):
+        return self.data.get(name, 1)
+
+    def show_all(self):
+        result = ''
+        for k, v in self.data.items():
+            result += f'{k}: ' + v.get_phone()
+            if not v.Birthday is None:
+                result += f' birthday {v.Birthday.value}'
+            result += '\n'
+        return result
+
+    def iterator(self, n=2):
+        key_list = list(self.data.keys())
+        if self.count + 1 >= len(key_list):
+            self.count = 0
+            return None
+        result_list = key_list[self.count: min(self.count + n, len(key_list))]
+        result = ''
+        for k in result_list:
+            result += f'{k}: ' + self.data[k].get_phone()
+            if not self.data[k].Birthday is None:
+                result += f' birthday {self.data[k].Birthday.value}'
+            result += '\n'
+            self.count += 1
+        yield result
+
+    def save_data(self):
+        with open('data.bin', 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def load_data(self):
+        with open('data.bin', 'rb') as f:
+            try:
+                data = pickle.load(f)
+                self.data = data
+            except EOFError:
+                print('File data is empty')
+
+    def find_all(self, value):
+        result = ''
+        for k, v in self.data.items():
+            found = False
+            if k.find(value) != -1:
+                found = True
+            for phone in v.Phones:
+                if phone.value.find(value) != -1:
+                    found = True
+            if found:
+                result += f'{k}: ' + v.get_phone()
+                if not v.Birthday is None:
+                    result += f' birthday {v.Birthday.value}'
+                result += '\n'
+        return result
---
+++ 12 week/dz 12/main.py
@@ -0,0 +1,98 @@
+from rich.prompt import Confirm
+from webencodings.x_user_defined import Codec
+
+from classes import AddressBook, Record, Name, Phone, Birthday
+from datetime import datetime
+
+CONTACT = AddressBook()
+
+
+def main():
+    say = input("Enter command: ")
+    exit_frase = (
+        'good bye',
+        'close',
+        'exit',
+        '.',
+    )
+    while not say.lower() in exit_frase:
+        command = say.split(' ')
+        if len(command) == 0:
+            print('You entered nothing')
+            say = input("Enter command: ")
+            continue
+        func_name = command[0].lower()
+        phone_str = ''
+        name_str = ''
+        try:
+            if func_name != 'show' and func_name != 'hello':
+                name_str = command[1]
+            if func_name == 'add' or func_name == 'change' or func_name == 'add_p' or func_name == 'add_b':
+                phone_str = command[2]
+        except IndexError:
+            print('Give me name and/or phone please')
+            say = input("Enter command: ")
+            continue
+
+        if func_name == 'hello':
+           print('How can I help you?')
+        elif func_name == 'add':
+            name = Name(name_str)
+            phone = Phone(phone_str)
+            birthday = None
+            if len(command) == 4:
+                birthday = Birthday(command[3])
+            record = Record(name, phone, birthday)
+            CONTACT.add_record(record)
+        elif func_name == 'add_b':
+            record = CONTACT.find_record(name_str)
+            if record != 1:
+                birthday = Birthday(phone_str)
+                record.Birthday = birthday
+        elif func_name == 'add_p':
+            record = CONTACT.find_record(name_str)
+            if record != 1:
+                phone = Phone(phone_str)
+                record.add_address(phone)
+        elif func_name == 'change':
+            record = CONTACT.find_record(name_str)
+            if record != 1:
+                record.delete_address(phone_str)
+                phone = Phone(phone_str)
+                record.add_address(phone)
+        elif func_name == 'phone':
+            record = CONTACT.find_record(name_str)
+            if record != 1:
+                print(record.get_phone())
+            else:
+                print('This name have no phone')
+        elif func_name == 'days_to_birthday':
+            record = CONTACT.find_record(name_str)
+            if record != 1:
+                print(record.days_to_birthday())
+        elif func_name == 'find':
+            print(CONTACT.find_all(name_str))
+        elif func_name == 'show':
+            func2 = command[1]
+            if func2 == 'all':
+                print(CONTACT.show_all())
+            else:
+                num = 2
+                try:
+                    num = int(func2)
+                except:
+                    print('Enter integer')
+                iterator = CONTACT.iterator(num)
+                for x in iterator:
+                    print(x)
+
+        else:
+            print("Function does not exist")
+        say = input("Enter command: ")
+    else:
+        CONTACT.save_data()
+        print('Good bye')
+
+
+if __name__ == '__main__':
+    main()
